c6c4d9ec672b5fa9a6a09d606fa88d56dc8083f2
straubventures <straub.ventures@gmail.com>
2021-03-25 12:43:20 -0700

    fixed templating

--- src/try2django/settings.py
+++ src/try2django/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'try2django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/straubfamily/cleanProj/try2Django/src/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
